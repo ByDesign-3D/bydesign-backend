469789d2b86fe1f81c73673dfcc4fff4b220ad6c
Tyler Alsop <tylerjalsop@gmail.com>
2021-03-01 16:30:58 -0900

    Updated the knexfile.js to connect to the database_url and migrations/seed files.

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,29 @@
-// Update with your config settings.
+var dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+  },
 };
