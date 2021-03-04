05c8f2cb45755c985d37c81cd2524407f1b2a0e9
Tyler Alsop <tylerjalsop@gmail.com>
2021-03-03 16:39:28 -0900

    Created a new migration for employees table and a seed file for that table. Created a .gitignore file and added the .env and employee seed file to it.

---
+++ zztemptables.js
@@ -0,0 +1,9 @@
+Employees
+
+id                               uuid
+first_name               string
+last_name               string
+username                string
+password                 string
+title                           string
+auth_level                     int
