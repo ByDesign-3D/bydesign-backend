268ef52479be129bf403be8cc418f71a9da617b4
Tyler Alsop <tylerjalsop@gmail.com>
2021-03-03 16:58:21 -0900

    gitignore should be working now.

--- zztemptables.js
+++
@@ -1,9 +0,0 @@
-Employees
-
-id                               uuid
-first_name               string
-last_name               string
-username                string
-password                 string
-title                           string
-auth_level                     int
