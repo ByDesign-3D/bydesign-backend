713012597ae9b8e50632831adb0d9728da7d73f8
Tyler Alsop <tylerjalsop@gmail.com>
2021-03-11 19:31:37 -0700

    Populated the employees table.

--- data/migrations/20210311190127_employees.js
+++ data/migrations/20210311190127_employees.js
@@ -1,8 +1,28 @@
-
-exports.up = function(knex) {
+exports.up = async function(knex) {
+    await knex.schema.createTable("employees", (table) => {
+        table.uuid("id")
+        table.text("first_name")
+            .notNullable()
+        table.text("last_name")
+            .notNullable()
+        table.text("title")
+        table.text("email")
+            .notNullable()
+            .unique()
+        table.string("phone_number")
+        table.integer("auth_level")
+        table.binary("image")
+        table.text("username")
+            .notNullable()
+            .unique()
+        table.text("password")
+            .notNullable()
+        
+    })
   
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("employees")
+
 };
