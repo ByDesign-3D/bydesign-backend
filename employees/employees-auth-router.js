d2028c05c43c9f90315a3cf970a7738b744fae94
Tyler Alsop <tylerjalsop@gmail.com>
2021-03-24 17:37:16 -0600

    Created an employee auth router using json web tokens.

---
+++ employees/employees-auth-router.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const Employees = require("./employees-model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const router = express.Router()
+
+router.post("/employee/register", async (req, res, next) => {
+    try {
+        const { username, password } = req.body
+        const employeeUserName = await Employees.findBy({ username }).first()
+
+        if (employeeUserName) {
+            return res.status(409).json({
+                message: "Sorry, that username already exists. Please choose a new username."
+            });
+        }
+        if ( username && password ) {
+            const newEmployee = await Employees.add({ username, password });
+            return res.status(201).json(newEmployee)
+        } else {
+            return res.status(500).json({ message: "Missing username or password." })
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+router.post("employee/login", async (req, res, next) => {
+    const authError = {
+        message: "Invalid Credentials.",
+    }
+    try {
+        const employee = await Employees.findBy({ username: req.body.username }).first()
+        if (!employee) {
+            return res.status(401).json(authError)
+        }
+        const passwordValid = await bcrypt.compare(req.body.password, employee.password)
+        if (!passwordValid) {
+            return res.status(401).json(authError)
+        }
+        const tokenPayload = {
+            employeeId: employee.id
+        }
+        const token = jwt.sign(tokenPayload, process.env.SECRET_KEY)
+        res.cookie("token", token)
+
+        res.json({
+            message: `Welcome ${employee.username}!`,
+            token: token,
+        })
+
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
