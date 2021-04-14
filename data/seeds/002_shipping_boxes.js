7cf7cf4682b43fa0215a06772746a744cb89a175
Tyler Alsop <tylerjalsop@gmail.com>
2021-04-13 18:32:17 -0600

    Finished seed files to setup the products table. Ready to open a pull request.

--- data/seeds/002_shipping_boxes.js
+++ data/seeds/002_shipping_boxes.js
@@ -1,6 +1,6 @@
 exports.seed = async function (knex) {
-  // Insert customers profiles
   await knex('shipping_boxes').insert([{
+    id: 1,
     brand: "USPS",
     size: "Medium",
     amount_in_stock: 15,
@@ -8,12 +8,14 @@ exports.seed = async function (knex) {
     
   },
   {
+    id: 2,
     brand: "UPS",
     size: "Jumbo",
     amount_in_stock: 5,
     cost_per_each: 13.75,
   },
   {
+    id: 3,
     brand: "Found on Side of Road",
     size: "12 X 16 X 6",
     units_of_measure: "Inches",
--- data/seeds/003_product_categories.js
+++ data/seeds/003_product_categories.js
@@ -1,29 +1,28 @@
 const faker = require('faker');
 
 exports.seed = async function (knex) {
-  // Insert customers profiles
   await knex('product_categories').insert([{
+    id: 1,
     name: "Miscellaneous",
     tagline: "Random products yet to be put into a category.",
     description: faker.commerce.productDescription(),
-    image: faker.image.image(),
-    image_url: "https://unsplash.com/photos/FB1vd3XT_zQ?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink",
+    image_url: "https://unsplash.com/photos/FB1vd3XT_zQ",
     video_url: "https://youtu.be/Ac5ksZTvZN8",
   },
   {
+    id: 2,
     name: "Kitchen Utensils",
     tagline: "Use them everyday!",
     description: faker.commerce.productDescription(),
-    image: faker.image.image(),
-    image_url: "https://unsplash.com/photos/yIaRpl_-Smw?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink",
+    image_url: "https://unsplash.com/photos/yIaRpl_-Smw",
     video_url: "https://youtu.be/W5O7TRTpesM",
   },
   {
+    id: 3,
     name: "Disney",
     tagline: "Where Dreams Come True",
     description: faker.commerce.productDescription(),
-    image: faker.image.image(),
-    image_url: "https://unsplash.com/photos/BLYq8Qk8Q98?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink",
+    image_url: "https://unsplash.com/photos/BLYq8Qk8Q98",
     video_url: "https://youtu.be/cPAbx5kgCJo",
   },
 ]);
